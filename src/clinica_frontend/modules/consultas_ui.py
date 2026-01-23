b8d78ee4950e8e374931fbffc8e3ae1318d9bde9
Miguel <ariasreymer10@gmail.com>
2026-01-22 21:37:32 -0500

    delete(frontend): Delete and Structure clinica_frontend for more efficient

--- src/clinica_frontend/modules/consultas_ui.py
+++
@@ -1,7 +0,0 @@
-
-# src/clinica_frontend/modules/consultas_ui.py
-import streamlit as st
-
-def show_consultas_ui():
-    st.title("Gestión de Consultas")
-    st.write("Funcionalidad en desarrollo.")
--- src/clinica_frontend/modules/inventario_ui.py
+++
@@ -1,7 +0,0 @@
-
-# src/clinica_frontend/modules/inventario_ui.py
-import streamlit as st
-
-def show_inventario_ui():
-    st.title("Gestión de Inventario")
-    st.write("Funcionalidad en desarrollo.")
